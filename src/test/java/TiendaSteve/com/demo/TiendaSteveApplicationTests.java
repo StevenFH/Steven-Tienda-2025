aa1c383ce9b28cf267e58fe8a653ffc66154c874
StevenFH <105827621+StevenFH@users.noreply.github.com>
2025-09-16 20:42:39 -0600

    Clase 1
    Prueba para realizar commits con los archivos compartidos por el profesor

---
+++ src/test/java/TiendaSteve/com/demo/TiendaSteveApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaSteve.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaSteveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
