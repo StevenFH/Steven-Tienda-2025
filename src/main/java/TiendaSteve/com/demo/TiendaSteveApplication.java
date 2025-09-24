95f96b186317aef2eba0c99cada58a700b56797e
StevenFH <105827621+StevenFH@users.noreply.github.com>
2025-09-23 20:16:24 -0600

    Commit 2 semana 3
    Se agrega "Fragmentos" en template.general
    
    Se agregan dependencias y se modifica el archivo Application
    
    Tambien se modifica el index de la pagina

--- src/main/java/TiendaSteve/com/demo/TiendaSteveApplication.java
+++ src/main/java/TiendaSteve/com/demo/TiendaSteveApplication.java
@@ -1,5 +1,12 @@
 package TiendaSteve.com.demo;
 
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -38,5 +45,36 @@ public class TiendaSteveApplication implements WebMvcConfigurer{
         resolver.setCheckExistence(true);
         return resolver;
     }
+        @Bean
+    public LocaleResolver localeResolver() {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.timezone");
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registro) {
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+
+    //Bean para poder acceder a los messages.properties en código...
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
        
 }
+
+
