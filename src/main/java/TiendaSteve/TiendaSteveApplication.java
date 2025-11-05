1c39ae439de5273f2685e3d47cee9ef271e1d8eb
StevenFH <105827621+StevenFH@users.noreply.github.com>
2025-11-04 19:52:49 -0600

    Commit Semana 9
    Actualización del Project Config
    
    Actualizacion del archivo general/fragmentos con el tema de la authenticacion
    
    Actualizacion de fragmentos

--- src/main/java/TiendaSteve/TiendaSteveApplication.java
+++ src/main/java/TiendaSteve/TiendaSteveApplication.java
@@ -23,6 +23,15 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 @SpringBootApplication
 public class TiendaSteveApplication implements WebMvcConfigurer{
@@ -31,7 +40,7 @@ public class TiendaSteveApplication implements WebMvcConfigurer{
 		SpringApplication.run(TiendaSteveApplication.class, args);
 	}
         
-            /* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
+    /* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
@@ -97,6 +106,84 @@ public class TiendaSteveApplication implements WebMvcConfigurer{
             return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
         }
     }
+    
+    public static final String[] PUBLIC_URLS = {
+        "/", "/index", "/fav/**", "/carrito/**", "/pruebas/**", "/registro/**",
+        "/js/**", "/webjars/**", "/login", "/acceso_denegado"
+    };
+
+    public static final String[] ADMIN_URLS = {
+        "/producto/nuevo", "/producto/guardar", "/producto/modificar/**", "/producto/eliminar/**",
+        "/categoria/nuevo", "/categoria/guardar", "/categoria/modificar/**", "/categoria/eliminar/**",
+        "/usuario/nuevo", "/usuario/guardar", "/usuario/modificar/**", "/usuario/eliminar/**"
+    };
+
+    public static final String[] ADMIN_OR_VENDEDOR_URLS = {
+        "/producto/listado", "/categoria/listado", "/usuario/listado"
+    };
+
+    public static final String[] USUARIO_URLS = {
+        "/facturar/carrito"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                .requestMatchers(PUBLIC_URLS).permitAll()
+                .requestMatchers(ADMIN_URLS).hasRole("ADMIN")
+                .requestMatchers(ADMIN_OR_VENDEDOR_URLS).hasAnyRole("ADMIN", "VENDEDOR")
+                .requestMatchers(USUARIO_URLS).hasRole("USUARIO")
+                .anyRequest().authenticated()
+        ).formLogin(form -> form // Configuración de formulario de login
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error=true")
+                .permitAll()
+        ).logout(logout -> logout // Configuración de logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll()
+        ).exceptionHandling(exceptions -> exceptions // Manejo de excepciones
+                .accessDeniedPage("/acceso_denegado")
+        ).sessionManagement(session -> session // Configuración de sesiones
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false)
+        );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //Este método será reemplazado la siguiente semana
+    @Bean
+    public UserDetailsService users(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder()
+                .username("juan")
+                .password(passwordEncoder.encode("123"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails sales = User.builder()
+                .username("rebeca")
+                .password(passwordEncoder.encode("456"))
+                .roles("VENDEDOR")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("pedro")
+                .password(passwordEncoder.encode("789"))
+                .roles("USUARIO") // Consistent con tu configuración
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, sales, user);
+    }
+
        
 }
 
