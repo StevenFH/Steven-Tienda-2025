96413743c3782dac6b7bcd221b61d6bc769f30e3
StevenFH <105827621+StevenFH@users.noreply.github.com>
2025-11-18 19:59:21 -0600

    Commit Semana 11
    Se realizan las siguientes modificaciones
    
    Agregar la seccion de Usuario
    Agregar la seccion de Registro
    Agregar la seccion de Ruta
    Agregar la seccion de Rol
    
    Adicional se agregaron los templates correspondientes a usuario y registro
    
    Finalmente se segmenta  el project config junta a StorageConfig y SecurityConfig

---
+++ src/main/java/TiendaSteve/services/RutaService.java
@@ -0,0 +1,19 @@
+package TiendaSteve.services;
+
+import TiendaSteve.domain.Ruta;
+import TiendaSteve.repository.RutaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RutaService {
+
+    @Autowired
+    private RutaRepository rutaRepository;
+
+    public List<Ruta> getRutas() {
+        return rutaRepository.findAllByOrderByRequiereRolAsc();
+    }
+
+}
